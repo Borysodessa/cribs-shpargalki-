253329244a8221f016cf8605dc20d78bca48e67b
BorysGull <chayka2021@yahoo.com>
2022-08-13 08:17:45 +0300

    добавил на libraries active

--- js/activeClass.js
+++ js/activeClass.js
@@ -1,8 +1,12 @@
 const title = document.querySelector('title');
-const link = document.querySelectorAll('a');
-console.log(link.getAttribute('href'));
-if(title.textContent === 'libraries' && link.getAttribute('href') === 'libraries'){
-    link.classList.add('active');
-}
-console.log(title.textContent)
+const link = document.querySelectorAll('a')
+
+link.forEach(linkEl => {
+    if(linkEl.getAttribute('href') === '../libraries/libraries.html'){
+        linkEl.classList.add('active');
+        console.log(linkEl.getAttribute('href'))
+    }}
+)
+
+
 
